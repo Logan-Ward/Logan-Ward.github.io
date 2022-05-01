d1f49855e0365d205fb3c3034a9ed0b36b36eeb2
Logan-Ward <ward_logan@outlook.com>
2022-05-01 18:42:45 +0000

    dhhds

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -3,7 +3,7 @@
 'use strict';
 
 var customers = require('./data/customers.json');
-var _ = require(/* Replace this with the name of your lodown! */);
+var _ = require('underbar');
 
 /**
  * 1. Import your lodown module using the require() method,
@@ -16,22 +16,62 @@ var _ = require(/* Replace this with the name of your lodown! */);
  *
  * 4. To test your work, run the following command in your terminal:
  *
- *    npm start --prefix ./<YOUR_GITHUB_FOLDER/projects/let-s-get-functional
+ *    npm start --prefix ./Logan-Ward.github.io/projects/let-s-get-functional
  *
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
 
-var maleCount = function(array) {
+var maleCount = function(array){
+    return _.filter(array, obj => obj.gender === 'male').length;
+}
 
-};
+var femaleCount = function(array){
+    return _.reduce(array, (count, obj) => obj.gender === 'female' ? ++count : count, 0);
+}
 
-var femaleCount;
+var oldestCustomer = function(array){
+    let oldest = 0;
+    let name = '';
+    for(let i = 0; i < array.length; i++){
+        if(array[i].age > oldest){
+            oldest = array[i].age;
+            name = array[i].name;
+        }
+    }
+    return name;
+}
 
-var oldestCustomer;
+var youngestCustomer = function(array){
+    let youngest = Infinity;
+    let name = '';
+    for(let i = 0; i < array.length; i++){
+        if(array[i].age < youngest){
+            youngest = array[i].age;
+            name = array[i].name;
+        }
+    }
+    return name;
+}
 
-var youngestCustomer;
-
-var averageBalance;
+var averageBalance = function(array){
+    let count = 0;
+    let sum = 0;
+    for(let i = 0; i < array.length; i++){
+        let strNum = '';
+        for(let j = 0; j < array[i].balance.length; j++){
+            console.log(parseInt(array[i].balance[j]));
+            if(typeof parseInt(array[i].balance[j]) === 'number' || array[i].balance[j] === '.'){
+                strNum += array[i].balance[j];
+            }
+        }
+        console.log(parseFloat(strNum));
+        sum += parseFloat(strNum);
+        count++;
+    }
+    console.log(sum);
+    return sum / count;
+}
+console.log(averageBalance(customers));
 
 var firstLetterCount;
 
